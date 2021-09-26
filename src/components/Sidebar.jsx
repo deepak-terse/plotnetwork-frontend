8f0fa473004fb5ef27c17098d83c5bf54f83be98
vaishalisagvekar-official <vaishalisagvekar.official@gmail.com>
2021-09-27 00:15:08 +0530

    commented settings tab

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -83,12 +83,12 @@ class Sidebar extends Component {
                     <span className="menu-title"><Trans>Projects</Trans></span>
                   </Link>
                 </li>
-                <li className={ this.isPathActive('/' + partner + '/settings') ? 'nav-item menu-items active' : 'nav-item menu-items' }>
+                {/* <li className={ this.isPathActive('/' + partner + '/settings') ? 'nav-item menu-items active' : 'nav-item menu-items' }>
                   <Link className="nav-link" to={"/" + partner + "/settings"}>
                     <span className="menu-icon"><i className="mdi mdi-settings"></i></span>
                     <span className="menu-title"><Trans>Settings</Trans></span>
                   </Link>
-                </li>
+                </li> */}
               </>
             : ""
           }
