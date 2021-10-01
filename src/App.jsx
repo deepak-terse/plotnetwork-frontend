8b4065e8216a2e39b9b7cc81d85befc4c25b9e73
vaishalisagvekar-official <vaishalisagvekar.official@gmail.com>
2021-10-02 00:11:07 +0530

    added condition in authenticating user

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,11 @@ import "./utils/i18n";
 import {gSheetInit} from './utils/gsheet_utils'
 
 function App() {
-	const isUserAuthenticated = localStorage.getItem("loggedInUser") ? true : false;
+	const user = JSON.parse(localStorage.getItem("loggedInUser"));
+	let isUserAuthenticated;
+	if(user){
+		isUserAuthenticated = (user.partnerDetails && user.projects) ? true : false;
+	}
 
 	gSheetInit().then(() => {
 		console.log("Success");
